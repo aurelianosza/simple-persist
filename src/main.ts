a37089d63837c006f5a7ffa9d6795ade4f933cd6
Aureliano Neto <aurelianosza@gmail.com>
2025-08-11 15:22:06 -0300

    enhacement: exporting default in main.

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,3 @@
 import { DataManagerFactory } from "./repository/baseDataManager";
-import { JsonDataManager } from "./repository/jsonDataManager";
 
-async function main() {
-
-    const repository = (new DataManagerFactory)
-        .type("csv")
-        .path(__dirname + "/../database/csv")
-        .entityName("users")
-        .headers(['id', 'name', 'birthdate'])
-        .create();
-
-    const rows = await repository.read([]);
-
-    console.log(rows);
-}
-
-main();
+export default new DataManagerFactory();
